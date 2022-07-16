81f0d144be838ce3dbeb3eb946ae03573bcd12a3
block-doc-au <neilc@cns-gc.com.au>
2022-07-16 15:40:45 +1000

    NC Deleted sub-folder and a few styling changes

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement('h2');
-h2.textContent = "This simple content added by Javascript... watch this  space";
+h2.textContent = "This simple content was added by Javascript... watch this  space";
 document.querySelector('body').appendChild(h2);
